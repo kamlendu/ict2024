b40c091b525679686a575cc1eaedc2cdbb7a0981
kamlendu <kamlendup@gmail.com>
2024-10-01 15:47:36 +0530

    java

---
+++ PublishingApp/src/main/java/it/publishingapp/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package it.publishingapp;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ PublishingApp/src/main/java/it/publishingapp/resources/JakartaEE8Resource.java
@@ -0,0 +1,20 @@
+package it.publishingapp.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("rest")
+public class JakartaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
